73371423d6b60ef354f5ee2679b04dec8c523b2b
dnscodnjs <dnscodnjs@naver.com>
2023-11-23 14:42:57 +0900

    회원가입 입력한 정보 컨트롤러로 전달

--- src/main/java/com/study/member/controller/MemberController.java
+++ src/main/java/com/study/member/controller/MemberController.java
@@ -2,13 +2,25 @@ package com.study.member.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
     // 회원가입 페이지 출력 요청
     @GetMapping("/member/save")
     public String saveForm(){
+        System.out.println("MemberController.saveForm");
         return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@RequestParam("memberEmail") String memberEmail,
+                       @RequestParam("memberPassword") String memberPassword,
+                       @RequestParam("memberName") String memberName){
+        System.out.println("MemberController.save");
+        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+        return "index";
     }
 
 }
